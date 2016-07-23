8a5c9cfe84292bbd80138653a2ed67ba211c6451
marcell <mars.rsjs@gmail.com>
2016-07-23 23:05:32 +0700

    penambahan menu kelola user role

---
+++ app/DataTables/UserOnRoleDataTable.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\UserOnRole;
+use Form;
+use Yajra\Datatables\Services\DataTable;
+
+class UserOnRoleDataTable extends DataTable
+{
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function ajax()
+    {
+        return $this->datatables
+            ->eloquent($this->query())
+            ->addColumn('action', 'userOnRoles.datatables_actions')
+            ->make(true);
+    }
+
+    /**
+     * Get the query object to be processed by datatables.
+     *
+     * @return \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder
+     */
+    public function query()
+    {
+        $userOnRoles = UserOnRole::query();
+
+        return $this->applyScopes($userOnRoles);
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\Datatables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+            ->columns($this->getColumns())
+            ->addAction(['width' => '10%'])
+            ->ajax('')
+            ->parameters([
+                'dom' => 'Bfrtip',
+                'scrollX' => false,
+                'buttons' => [
+                    'create',
+                    'print',
+                    'reset',
+                    'reload',
+                    [
+                         'extend'  => 'collection',
+                         'text'    => '<i class="fa fa-download"></i> Export',
+                         'buttons' => [
+                             'csv',
+                             'excel',
+                             'pdf',
+                         ],
+                    ]
+                ]
+            ]);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    private function getColumns()
+    {
+        return [
+            'users' => ['name' => 'users', 'data' => 'users'],
+            'roles' => ['name' => 'roles', 'data' => 'roles'],
+            'deleted_at' => ['name' => 'deleted_at', 'data' => 'deleted_at'],
+            'created_at' => ['name' => 'created_at', 'data' => 'created_at'],
+            'updated_at' => ['name' => 'updated_at', 'data' => 'updated_at']
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'userOnRoles';
+    }
+}
---
+++ app/Models/UserOnRole.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @SWG\Definition(
+ *      definition="UserOnRole",
+ *      required={},
+ *      @SWG\Property(
+ *          property="id",
+ *          description="id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="users",
+ *          description="users",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="roles",
+ *          description="roles",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="deleted_at",
+ *          description="deleted_at",
+ *          type="string"
+ *      )
+ * )
+ */
+class UserOnRole extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'user_on_role';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+    protected $dates = ['deleted_at'];
+
+
+    public $fillable = [
+        'users',
+        'roles',
+        'deleted_at'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'users' => 'string',
+        'roles' => 'string',
+        'deleted_at' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        
+    ];
+}
